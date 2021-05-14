165d250b4700e4ed1b0c53262872af9fca3ef216
Mariposa Libre <angeles.vargas@ymail.com>
2021-05-14 13:43:15 -0600

    Initial commit

---
+++ src/git04/HelloFromRachel.java
@@ -0,0 +1,15 @@
+package git04;
+
+ public class HelloFromRachel {
+    public void sayHello() {
+        System.out.println("Hello, my name is Rachel");
+
+    }
+        public static void main(String[] args) {
+            // write your code here
+            HelloFromRachel hello = new HelloFromRachel();
+            hello.sayHello();
+
+        }
+    }
+
