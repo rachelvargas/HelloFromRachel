0ee0f05c1f934a38c02022ade7c48f7e39619899
alaredosr <alaredosr@hotmail.com>
2021-05-22 15:10:58 -0600

    My hello line was added

--- src/git04/HelloFromRachel.java
+++ src/git04/HelloFromRachel.java
@@ -3,6 +3,7 @@ package git04;
  public class HelloFromRachel {
     public void sayHello() {
         System.out.println("Hello, my name is Rachel");
+        System.out.println("Hello Rachel, my name is Armando");
 
     }
         public static void main(String[] args) {
